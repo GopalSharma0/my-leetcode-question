abb7b339227ef82c81d5bbdc5ae8dfda70ab17a0
Gopal sharma <85856661+GopalSharma0@users.noreply.github.com>
2023-06-11 09:38:57 +0530

    Time: 4 ms (83.58%), Space: 10.5 MB (99.01%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,31 +1,25 @@
 class Solution {
-public:
-vector<vector<int>>ans;
-void help(int i,vector<int>arr,int target,int sum,vector<int>&subset){
-
-if(sum==target){
-    ans.push_back(subset);
-    return ;
-}
-if(sum>target) return;
-if(i>=arr.size()) return ;
-help(i+1,arr,target,sum,subset);
-
-sum+=arr[i];
-subset.push_back(arr[i]);
-help(i,arr,target,sum,subset);
-sum-=arr[i];
-subset.pop_back();
-
-
-}
-
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>subset;
+    void combination(const vector<int>& candidates, int target, vector<int> &currComb, int currSum, int currIndex, vector<vector<int>>& ans){
+        if(currSum>target) return; //backtrack
+        if(currSum==target){
+            ans.push_back(currComb); //store the solution and backtrack
+            return;
+        }
         
-        int sum=0;
-        help(0,candidates,target,sum,subset);
-       return ans;
+        for(int i=currIndex; i<candidates.size(); i++){ //try all possible options for the next level
+            currComb.push_back(candidates[i]); //put 1 option into the combination
+            currSum+=candidates[i];
+            combination(candidates, target, currComb, currSum, i, ans); //try with this combination, whether it gives a solution or not.
+            currComb.pop_back(); //when this option backtrack to here, remove this and go on to the next option.
+            currSum-=candidates[i];
+        }
+        
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> currComb;
+        combination(candidates, target, currComb, 0, 0, ans);
+        return ans;
     }
 };
